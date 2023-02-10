20a86d8cfef8368db9dbf4513b4895625bfa9981
Michelle_Mungai <chellymungai@gmail.com>
2023-02-10 22:35:24 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
-
+    attr_accessor :name
+    attr_accessor :breed
+    def names (name)
+        @name = name
+    end
+    def breeds (breed)
+        @breed = breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-
+    attr_accessor :name
+    attr_accessor :job
+    def names(name)
+        @name = name
+    end
+    def jobs (job)
+        @job = job
+    end
 end
